bbd7141c42052941e5b3af985ba53588bbc4bc08
LAPTOP-ULCU732A\User <valeri.ignateva@list.ru>
2023-02-13 14:36:24 +0300

    Добавьте файлы проекта.

---
+++ Lab1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число: ");
+            double Number = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите степень: ");
+            double Pow = Double.Parse(Console.ReadLine());
+
+            double Result = Math.Exp(Pow * Math.Log(Number));
+
+            Console.WriteLine($"Результат: {Result}");
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ Lab2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число: ");
+            string Number = Console.ReadLine();
+
+            if (Number.Length > 1)
+            {
+                char[] Digits = Number.ToCharArray();
+                char SecondDigits = Digits[1];
+                Digits[1] = Digits[Digits.Length - 1];
+                Digits[Digits.Length - 1] = SecondDigits;
+                Console.WriteLine($"Измененное число: {new string(Digits)}");
+                Console.ReadLine();
+            }
+        }
+    }
+}
